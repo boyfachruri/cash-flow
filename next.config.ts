523e38a9725c0175991f2052f22c3a00fcbb8b15
boyfachruri <boyfachruri@gmail.com>
2025-02-25 11:29:17 +0700

    dfsdfd

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
-import type { NextConfig } from 'next';
+// import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  // Pengaturan lainnya
-  middleware: {
-    '/main': ['middleware'], // Hanya rute /main yang dilindungi middleware
-  },
-};
+// const nextConfig: NextConfig = {
+//   // Pengaturan lainnya
+//   middleware: {
+//     '/main': ['middleware'], // Hanya rute /main yang dilindungi middleware
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Nonaktifkan PWA di development
+});
+
+module.exports = withPWA({
+  // Konfigurasi Next.js lainnya
+});
+
+
+
